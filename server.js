67a630ce9fac0846f626ed802649e971608c0df7
garyng13 <wrc2979@autuni.ac.nz>
2020-08-14 15:55:55 +1200

    4 change

--- server.js
+++ server.js
@@ -8,10 +8,5 @@ app.listen(PORT, ()=>{
 });
 
 app.get('/', (req, res) =>{
-<<<<<<< HEAD
-	res.send({ok: false});
-})
-=======
 	res.send({ok: true});
 })
->>>>>>> 383f28846dc81b879cf091541d5a37e8a5a0b0f1
