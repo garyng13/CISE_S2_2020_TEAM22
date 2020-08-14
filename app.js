15b611557f5edb848fd030c29dd79ad5592989be
garyng13 <wrc2979@autuni.ac.nz>
2020-08-14 22:19:29 +1200

    venus garys

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 module.exports ={
 	sayGary: function(){
-		return 'gary';
+		return 'garys';
 	},
 	addNumbers: function(a,b){
 		return a+b;
--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.listen(PORT, ()=>{
 });
 
 app.get('/', (req, res) =>{
-	res.send({ok: false, text: "hi venus VV"});
+	res.send({ok: false, text: "hi venus garys"});
 })
