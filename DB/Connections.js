1e89ba69c040046f63175f16797aea8aedc09e6d
garyng13 <wrc2979@autuni.ac.nz>
2020-09-29 10:54:02 +1300

    connected cloud mongoDB key removed

--- DB/Connections.js
+++ DB/Connections.js
@@ -1,7 +1,9 @@
+const config = require('../config.js');
+
 const mongoose = require('mongoose');
 
 //second trial to connect to cloud based mongodb
-const url = 'mongodb+srv://dbUser:GaryAut2020@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
+const url = 'mongodb+srv://dbUser:'+config.KEY+'@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
 
 const connectDB = async () =>{
 	await mongoose.connect(url,{useUnifiedTopology:true,useNewUrlParser:true});
