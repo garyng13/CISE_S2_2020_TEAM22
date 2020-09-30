8457387ff5c4dae65967c4f3f910ce97bc51aef5
garyng13 <wrc2979@autuni.ac.nz>
2020-09-30 21:40:31 +1300

    not using env variable

--- DB/Connections.js
+++ DB/Connections.js
@@ -3,9 +3,9 @@ const config = require('../config.js');
 const mongoose = require('mongoose');
 
 //second trial to connect to cloud based mongodb
-//const url = 'mongodb+srv://dbUser:'+config.KEY+'@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
+const url = 'mongodb+srv://dbUser:'+config.KEY+'@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
 
-const url = process.env.url;
+//const url = process.env.url;
 
 const connectDB = async () =>{
 	await mongoose.connect(url,{useUnifiedTopology:true,useNewUrlParser:true});
