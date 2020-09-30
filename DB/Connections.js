0395d481208550357e08a4d483475cb651c54972
garyng13 <wrc2979@autuni.ac.nz>
2020-10-01 00:17:56 +1300

    change to use remote mongo url

--- DB/Connections.js
+++ DB/Connections.js
@@ -3,10 +3,10 @@ const config = require('../config.js');
 const mongoose = require('mongoose');
 
 //url to connect mongodb Atlas
-//const url = process.env.MONGO_URL ||'mongodb+srv://dbUser:'+config.KEY+'@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
+const url = process.env.MONGO_URL ||'mongodb+srv://dbUser:'+config.KEY+'@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
 
 
-const url = "mongodb://localhost:27017/seer";
+//const url = "mongodb://localhost:27017/seer";
 //const url = process.env.url;
 
 const connectDB = async () =>{
