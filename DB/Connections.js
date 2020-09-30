563e2a0a2ac0d8688f15e03f59bfa5a8e9f2601e
garyng13 <wrc2979@autuni.ac.nz>
2020-10-01 10:32:17 +1300

    not using config.js

--- DB/Connections.js
+++ DB/Connections.js
@@ -1,9 +1,10 @@
-const config = require('../config.js');
+//const config = require('../config.js');
 
 const mongoose = require('mongoose');
 
 //url to connect mongodb Atlas
-const url = process.env.MONGO_URL ||'mongodb+srv://dbUser:'+config.KEY+'@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
+//const url = process.env.MONGO_URL ||'mongodb+srv://dbUser:'+config.KEY+'@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
+const url = process.env.MONGO_URL ||'mongodb+srv://dbUser:GaryAut2020@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
 
 
 //const url = "mongodb://localhost:27017/seer";
