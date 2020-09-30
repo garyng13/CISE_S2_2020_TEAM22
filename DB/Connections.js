0410e29e468b2159ffafc35a7d41ea6c586e6653
garyng13 <wrc2979@autuni.ac.nz>
2020-09-30 23:12:09 +1300

    add config var

--- DB/Connections.js
+++ DB/Connections.js
@@ -3,7 +3,7 @@ const config = require('../config.js');
 const mongoose = require('mongoose');
 
 //url to connect mongodb Atlas
-const url = 'mongodb+srv://dbUser:'+config.KEY+'@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
+const url = process.env.MONGO_DB ||'mongodb+srv://dbUser:'+config.KEY+'@cluster0.npid8.mongodb.net/seer?retryWrites=true&w=majority';
 
 //const url = process.env.url;
 
